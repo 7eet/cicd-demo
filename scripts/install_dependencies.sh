2de27fbed662ae2a75b4cf26af3634c567922b1b
7eet <raisimranjeetsingh@gmail.com>
2022-01-11 11:29:53 +0530

    added script for secret manager

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-aws ssm get-parameter     --name /DB/NAME
+DBNAME=$(aws ssm get-parameters --region ap-south-1 --names /DB/NAME --with-decryption --query Parameters[0].Value)
+DBPSSWD=$(aws ssm get-parameters --region ap-south-1 --names /DB/PSSWD --with-decryption --query Parameters[0].Value)
+SERVICEURL=$(aws ssm get-parameters --region ap-south-1 --names /SERVICE/DUMMYURL --with-decryption --query Parameters[0].Value)
+
+length=${#DBNAME}
+
+DBNAME=${DBNAME:1:$(($length-1))}
+
+length=${#DBPSSWD}
+
+DBPSSWD=${DBPSSWD:1:$(($length-1))}
+
+length=${#SERVICEURL}
+
+SERVICEURL=${SERVICEURL:1:$(($length-1))}
+
+echo "DBNAME is ${DBNAME}"
+echo "DBPSSWD is ${DBPSSWD}"
+echo "SERVICEURL is ${SERVICEURL}"
+
 cd /home/ubuntu
 npm install pm2 -g
 npm i
